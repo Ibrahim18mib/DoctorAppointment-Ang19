b08f56be44a6e1ec25264ceb59fe6853da401523
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2025-07-23 00:21:42 -1200

    docker setups and some modifications in json files

--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MasterService {
-  baseUrl: any = 'https://localhost:7106/';
+  // baseUrl: any = 'https://localhost:7106/';
+  baseUrl: any = environment.API_BASE_URL;
   constructor(private http: HttpClient) {}
 
   getAllAppointments() {
